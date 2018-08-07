60867862d64fc3f2d505e98109b77c3372d40942
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-07 14:33:49 +0530

    test cases corrected

--- src/test/java/com/rest/controller/LocalSearchDirectoryApplicationTests.java
+++ src/test/java/com/rest/controller/LocalSearchDirectoryApplicationTests.java
@@ -3,14 +3,22 @@ package com.rest.controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.example.LocalSearchDirectoryApplication;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {LocalSearchDirectoryApplication.class})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class LocalSearchDirectoryApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
+	@Test
+	public void canaryTest() {
+		assert(true);
+	}
 }
