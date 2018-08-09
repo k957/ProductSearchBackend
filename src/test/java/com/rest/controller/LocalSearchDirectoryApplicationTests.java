709dba2a4113d0f958d68b5e08e960879a781b97
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-09 11:52:15 +0530

    application.properties with db deatils

--- src/test/java/com/rest/controller/LocalSearchDirectoryApplicationTests.java
+++ src/test/java/com/rest/controller/LocalSearchDirectoryApplicationTests.java
@@ -1,19 +1,16 @@
-/*package com.rest.controller;
+package com.rest.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.LocalSearchDirectoryApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {LocalSearchDirectoryApplication.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class LocalSearchDirectoryApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
@@ -22,4 +19,3 @@ public class LocalSearchDirectoryApplicationTests {
 		assert(true);
 	}
 }
-*/
