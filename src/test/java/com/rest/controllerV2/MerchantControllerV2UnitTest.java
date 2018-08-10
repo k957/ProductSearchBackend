d948c811f75105b494e072dc1b201c3a34232164
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-10 10:29:17 +0530

    Test cases written for merchant api

---
+++ src/test/java/com/rest/controllerV2/MerchantControllerV2UnitTest.java
@@ -0,0 +1,100 @@
+package com.rest.controllerV2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.LocalSearchDirectoryApplication;
+import com.example.dto.MerchantDto;
+import com.example.service.IMerchantService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LocalSearchDirectoryApplication.class)
+public class MerchantControllerV2UnitTest {
+	@Autowired
+	private WebApplicationContext wac;
+	private MockMvc mvc;
+
+	@Autowired
+	private IMerchantService merchantService;
+	
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void testViewAll() {
+		MerchantDto merchant = new MerchantDto();
+		merchant.setName("kanav");
+		merchant.setDisplayName("kkk");
+		merchant.setMailId("kanav@gmail.com");
+		merchant.setStatus('A');
+		merchant.setMobileNo("9999");
+		Mockito.when(merchantService.create(merchant)).thenReturn("hdhd");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/v2/merchant")
+				.contentType(MediaType.APPLICATION_JSON);
+		try {
+			MvcResult result = mvc.perform(requestBuilder).andReturn();
+			assertEquals(200, result.getResponse().getStatus());
+			assertEquals("application/json;charset=UTF-8", result.getResponse().getContentType());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	@Test
+	public void testViewOne() {
+		MerchantDto merchant = new MerchantDto();
+		merchant.setName("kanav");
+		merchant.setDisplayName("kkk");
+		merchant.setMailId("kanav@gmail.com");
+		merchant.setStatus('A');
+		merchant.setMobileNo("9999");
+		merchantService.create(merchant);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/v2/merchant/merchantId/2")
+				.contentType(MediaType.APPLICATION_JSON);
+		try {
+			MvcResult result = mvc.perform(requestBuilder).andReturn();
+			assertEquals(200, result.getResponse().getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testViewByDisplayName() {
+		MerchantDto merchant = new MerchantDto();
+		merchant.setName("kanav");
+		merchant.setDisplayName("kkk");
+		merchant.setMailId("kanav@gmail.com");
+		merchant.setStatus('A');
+		merchant.setMobileNo("9999");
+		merchantService.create(merchant);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/v2/merchant/displayName/kkk")
+				.contentType(MediaType.APPLICATION_JSON);
+		try {
+			MvcResult result = mvc.perform(requestBuilder).andReturn();
+			assertEquals(200, result.getResponse().getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
