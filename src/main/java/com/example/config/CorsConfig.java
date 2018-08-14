a535e57999e8bc714864c1ed96f22141fc045090
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-14 14:34:54 +0530

    cors update 3

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,17 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig{
 	  @Bean
 	    public WebMvcConfigurer corsConfigurer() {
 	        return new WebMvcConfigurerAdapter() {
 	        
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("**/v2/**").allowedOrigins("http://184.72.75.108:8080","http://172.18.73.160:4200","http://172.18.73.160:8080");
-//	                registry.addMapping("**/v2**").allowedOrigins();
-//	                registry.addMapping("**/v2/**").allowedOrigins();
+	                registry.addMapping("**/v2/**").allowedOrigins("http://184.72.75.108:8080/**","http://172.18.73.160:4200/**","http://172.18.73.160:8080/**")
+	                .allowedMethods("GET, POST, DELETE, PUT, OPTIONS, HEAD")
+	                .allowedHeaders("Content-Type, Accept, X-Requested-With");
 	            }
 	        };
 	    }
+
+	
+
+	
 }
