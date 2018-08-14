a2f0b499d4e3be80678ec8da9ad2273c45421e6f
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-14 17:04:24 +0530

    cors update 9

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -1,13 +1,14 @@
-/*package com.example.config;
+package com.example.config;
 
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+@EnableWebMvc
 @Configuration
 public class CorsConfig{
 	  @Bean
@@ -15,12 +16,12 @@ public class CorsConfig{
 	        return new WebMvcConfigurerAdapter() {
 	        
 	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {*/
-	                //registry.addMapping("**/v2/**")
-	                /*.allowedOrigins("*")
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("**/v2/**")
+	                .allowedOrigins("*")
 	                .allowedMethods("GET, POST, DELETE, PUT, OPTIONS, HEAD")
 	                .allowedHeaders("Content-Type, Accept, X-Requested-With");
 	            }
 	        };
 	    }
-}*/
+}
--- src/main/java/com/example/controller/MerchantControllerV2.java
+++ src/main/java/com/example/controller/MerchantControllerV2.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,7 @@ import com.example.service.IMerchantService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-//@CrossOrigin(origins = "http://184.72.75.108:8080", allowedHeaders="GET, POST, DELETE, PUT, OPTIONS, HEAD")
+//@CrossOrigin(origins = "http://184.72.75.108:8080", allowedHeaders= {"Content-Type, Accept, X-Requested-With"},methods)
 @RestController
 @RequestMapping("/v2/merchant")
 @Api(value="Merchant Controller REST Endpoint unsecured version 2",description="Merchant Info API")
