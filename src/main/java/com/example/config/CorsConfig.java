fe5266808558022d4c6e1572d3971891988957db
Kanav Mehra <kanavmehra957@gmail.com>
2018-08-14 14:58:01 +0530

    cors update 4

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,7 +16,7 @@ public class CorsConfig{
 	        
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("**/v2/**").allowedOrigins("http://184.72.75.108:8080/**","http://172.18.73.160:4200/**","http://172.18.73.160:8080/**")
+	                registry.addMapping("**/v2/**").allowedOrigins("*")
 	                .allowedMethods("GET, POST, DELETE, PUT, OPTIONS, HEAD")
 	                .allowedHeaders("Content-Type, Accept, X-Requested-With");
 	            }
